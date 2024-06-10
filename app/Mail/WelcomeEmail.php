6cd7cf547e0c4e76ccfbee0415f555b56306e382
Mohsin <mohsin@larasoft.io>
2024-06-10 08:02:38 +0500

    minor

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -1,35 +1,33 @@
-<?php
+    <?php
+    namespace App\Mail;
+    use Illuminate\Bus\Queueable;
+    use Illuminate\Contracts\Queue\ShouldQueue;
+    use Illuminate\Mail\Mailable;
+    use Illuminate\Queue\SerializesModels;
 
-namespace App\Mail;
-
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
-
-class WelcomeEmail extends Mailable
-{
-    use Queueable, SerializesModels;
-    protected $user;
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct($user)
+    class WelcomeEmail extends Mailable
     {
-        $this->user = $user;
-    }
+        use Queueable, SerializesModels;
+        protected $user;
+        /**
+         * Create a new message instance.
+         *
+         * @return void
+         */
+        public function __construct($user)
+        {
+            $this->user = $user;
+        }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
-    {
-        return $this->from('noreply@example.com', 'Your App') // Avoid setting from here if you want to use default configuration
-        ->view('emails.welcome_email')
-            ->with(['user' => $this->user]);
+        /**
+         * Build the message.
+         *
+         * @return $this
+         */
+        public function build()
+        {
+            return $this->from('noreply@example.com', 'Your App') // Avoid setting from here if you want to use default configuration
+            ->view('emails.welcome_email')
+                ->with(['user' => $this->user]);
+        }
     }
-}
